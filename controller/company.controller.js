fbd0169b6d0eb1a2e2316b90957ece0873a13fcf
shubham-wawale <wawaleshubham@gmail.com>
2023-03-25 16:10:06 +0530

    Commit

--- controller/company.controller.js
+++ controller/company.controller.js
@@ -13,7 +13,7 @@ companyController.post("/login", (req, res) => {
       if (company.hashedPassword != sha256(password)) {
         res.json({ success: false, message: "Incorrect Password" })
       } else {
-        res.json({ success: true, companyId: company._id, message:"Company successfully logged in."})
+        res.json({ success: true, companyData: company, message: "Company successfully logged in." })
       }
     }
   })
@@ -40,34 +40,34 @@ companyController.post("/signup", (req, res) => {
       vision
     };
     CompanyInfo.find({ email: email }).then(company => {
-      if (company.length==0) {
+      if (company.length == 0) {
         const newCompany = new CompanyInfo(companyData);
         newCompany
           .save()
           .then(data => {
-            res.status(200).json({ success: true, companyId: data._id, message:"Company registration successful." });
+            res.status(200).json({ success: true, companyId: data._id, message: "Company registration successful." });
           })
           .catch(err => {
-            res.status(404).json({success: false, message : "Company registration unsuccessful"});
+            res.status(404).json({ success: false, message: "Company registration unsuccessful" });
           })
       } else {
-        res.status(200).json({success: false, message: `Company with the email ${email} already exists.`});
+        res.status(200).json({ success: false, message: `Company with the email ${email} already exists.` });
       }
     })
   }
   else {
-    res.json({message: "Invalid request object."});
+    res.json({ message: "Invalid request object." });
   }
 
 });
 
-companyController.get("/driveInfo", (req,res)=> {
-  const {driveId} = req.query;
-  CompanyDrive.find({_id: driveId }).then(drive=> {
+companyController.get("/driveInfo", (req, res) => {
+  const { driveId } = req.query;
+  CompanyDrive.find({ _id: driveId }).then(drive => {
     if (!drive) {
       res.json({ success: false, message: "No drive found." })
     } else {
-      res.json({ success: true,message: "Drive retrieved",  drive: drive })
+      res.json({ success: true, message: "Drive retrieved", drive: drive })
     }
   })
 })
@@ -89,42 +89,156 @@ companyController.get("/appliedStudentsDrive", (req, res) => {
     if (!drive) {
       res.json({ success: false, message: "No drives found." })
     } else {
-      res.json({ success: true, appliedStudents : drive.appliedStudents })
+      res.json({ success: true, appliedStudents: drive.appliedStudents })
     }
   })
 })
 
-companyController.post("/addStudentToDrive", (req,res) => {
-  const {studentData, driveId} = req.body
-  if (CompanyDrive.find({_id: driveId, appliedStudents: { $in: [studentData._id]} }).count() == 1) {
-    res.json({
-      success: false,
-      message: "Already applied to the drive."
-    })
-  } else {
-  CompanyDrive.updateOne({_id: driveId}, { $push: {appliedStudents: studentData} })
-    .then(update => {
-      update.matchedCount==1 ? res.status(200).json({ 
-        success: true,
-        message: `Student applied to drive successfully.` })
-      : res.json({ success: false, message: "Could not apply to drive." })
-    }).catch(err => {
-      res.json({ success: false, message: err })
-    })
-  }
+companyController.post("/addStudentToDrive", (req, res) => {
+  const { studentData, driveId } = req.body
+
+  CompanyDrive.find({ _id: driveId, "appliedStudents.id": studentData.id }).then(document => {
+
+    if (document.length == 1) {
+      res.json({
+        success: false,
+        message: "Already applied to the drive."
+      })
+    } else {
+      CompanyDrive.updateOne({ _id: driveId }, { $push: { appliedStudents: studentData } })
+        .then(update => {
+          update.n == 1 ? res.status(200).json({
+            success: true,
+            message: `Student applied to drive successfully.`
+          })
+            : res.json({ success: false, message: "Could not apply to drive." })
+        }).catch(err => {
+          res.json({ success: false, message: err })
+        })
+    }
+  })
 })
 
-companyController.post("/removeStudentFromDrive", (req,res)=> {
-  const {studentId, driveId} = req.body
-  CompanyDrive.updateOne({_id: driveId}, {$pull: {appliedStudents:  {id: studentId}}})
-  .then(update => {
-    update.matchedCount==1 ? res.status(200).json({ 
-      success: true,
-      message: `Student removed from drive successfully.` })
-    : res.json({ success: false, message: "Could not delete student." })
+companyController.post("/filterStudentsByTest",async  (req, res) => {
+  const { studentEmails, driveId, key } = req.body
+
+  const query = {_id: driveId }
+
+  const updateClearedTest = {
+    $set: { 
+      "appliedStudents.$[element].testCleared" : true,
+     }
+  }
+
+  const updateNotClearedTest = {
+    $set: { 
+      "appliedStudents.$[element].rejected" : true,
+     }
+  }
+
+  const rejectedEmailsFilter = {
+    arrayFilters: [
+      {
+        "element.email": {$in: studentEmails},
+      },
+    ],
+  }
+  const acceptedEmailsFilter = {
+    arrayFilters: [
+      {
+        "element.email": {$nin: studentEmails},
+      },
+    ],
+  }
+  CompanyDrive.updateOne(query, updateNotClearedTest, rejectedEmailsFilter)
+    .then(firstUpdate => {
+    console.log(firstUpdate)
+    if(firstUpdate.n >=1 ) {
+      CompanyDrive.updateOne(query, updateClearedTest, acceptedEmailsFilter)
+      .then(secondUpdate=>{
+        console.log(secondUpdate)
+        secondUpdate.n >= 1 ? res.status(200).json({
+          success: true,
+          message: `Students filtered in drive successfully by test.`
+        })
+          : res.json({ success: false, message: "Could not update students." })
+      })
+    }
   }).catch(err => {
     res.json({ success: false, message: err })
   })
+})
+
+companyController.post("/filterStudentsByInterview",async  (req, res) => {
+  const { studentEmails, driveId } = req.body
+  
+  const query = {_id: driveId }
+
+  const updateClearedInterview = {
+    $set: { 
+      "appliedStudents.$[element].interviewCleared" : true,
+     }
+  }
+
+  const updateNotClearedInterview = {
+    $set: { 
+      "appliedStudents.$[element].rejected" : true,
+     }
+  }
+
+  const rejectedEmailsFilter = {
+    arrayFilters: [
+      {
+        "element.email": {$in: studentEmails},
+      },
+    ],
+  }
+  const acceptedEmailsFilter = {
+    arrayFilters: [
+      {
+        "element.email": {$nin: studentEmails},
+      },
+    ],
+  }
+  CompanyDrive.updateOne(query, updateNotClearedInterview, rejectedEmailsFilter)
+    .then(firstUpdate => {
+    console.log(firstUpdate)
+    if(firstUpdate.n >=1 ) {
+      CompanyDrive.updateOne(query, updateClearedInterview, acceptedEmailsFilter)
+      .then(secondUpdate=>{
+        console.log(secondUpdate)
+        secondUpdate.n >= 1 ? res.status(200).json({
+          success: true,
+          message: `Students filtered in drive successfully by interview.`
+        })
+          : res.json({ success: false, message: "Could not update students." })
+      })
+    }
+  }).catch(err => {
+    res.json({ success: false, message: err })
+  })
+})
+
+companyController.post("/removeStudentFromDrive", (req, res) => {
+  const { studentId, driveId } = req.body
+  // console.log("studentId", studentId)
+  // console.log(driveId)
+  CompanyDrive.updateOne({ _id: driveId, "appliedStudents.id": studentId }, {
+    $set: {
+      "appliedStudents.$.rejected": true,
+      "appliedStudents.$.status": "Rejected",
+    }
+  })
+    .then(update => {
+      // console.log(update)
+      update.n == 1 ? res.status(200).json({
+        success: true,
+        message: `Student removed from drive successfully.`
+      })
+        : res.json({ success: false, message: "Could not delete student." })
+    }).catch(err => {
+      res.json({ success: false, message: err })
+    })
 })
 
 
@@ -133,7 +247,7 @@ companyController.post("/createDrive", (req, res) => {
   const { companyId,
     driveName,
     branchesPreferred,
-    batch, 
+    batch,
     tenthPercentage,
     twelfthPercentage,
     cgpa,
@@ -150,43 +264,44 @@ companyController.post("/createDrive", (req, res) => {
     skillsRequired } = req.body;
 
   const companyDriveData = {
-    companyId:companyId,
-    driveName:driveName,
-    branchesPreferred:branchesPreferred,
-    batch:batch, 
-    tenthPercentage:tenthPercentage,
-    twelfthPercentage:twelfthPercentage,
-    cgpa:cgpa,
-    cgpaInPercentage:cgpaInPercentage,
-    numberOfLiveKT:numberOfLiveKT,
-    numberOfDeadKT:numberOfDeadKT,
-    numberOfAcademicGaps:numberOfAcademicGaps,
-    numberOfDegreeGaps:numberOfDegreeGaps,
-    ctcOffered:ctcOffered,
-    jobDescription:jobDescription,
-    jobRole:jobRole,
-    jobType:jobType,
-    jobLocation:jobLocation.split(","),
-    skillsRequired:skillsRequired.split(",")
+    companyId: companyId,
+    driveName: driveName,
+    branchesPreferred: branchesPreferred,
+    batch: batch,
+    tenthPercentage: tenthPercentage,
+    twelfthPercentage: twelfthPercentage,
+    cgpa: cgpa,
+    cgpaInPercentage: cgpaInPercentage,
+    numberOfLiveKT: numberOfLiveKT,
+    numberOfDeadKT: numberOfDeadKT,
+    numberOfAcademicGaps: numberOfAcademicGaps,
+    numberOfDegreeGaps: numberOfDegreeGaps,
+    ctcOffered: ctcOffered,
+    jobDescription: jobDescription,
+    jobRole: jobRole,
+    jobType: jobType,
+    jobLocation: jobLocation.split(","),
+    skillsRequired: skillsRequired.split(",")
   };
 
   const newDrive = new CompanyDrive(companyDriveData);
   newDrive
     .save()
     .then(data => {
-      res.status(200).json({success: true, message:"Drive added successfully"});
+      res.status(200).json({ success: true, message: "Drive added successfully" });
     })
     .catch(err => {
-      res.status(400).json({success: false, message:"Unable to add drive",error: err});
+      res.status(400).json({ success: false, message: "Unable to add drive", error: err });
     })
 });
 
 companyController.post("/updateDrive", (req, res) => {
   !req.body.driveId ? res.json({ success: false, error: "Drive Id not recieved." }) :
     CompanyDrive.updateOne({ _id: req.body.driveId }, req.body.updatedData).then(err => {
-      err.matchedCount == 1 ? res.status(200).json({ 
-          success: true,
-          message: `Updated drive ID ${req.body.driveId} successfully.` })
+      err.matchedCount == 1 ? res.status(200).json({
+        success: true,
+        message: `Updated drive ID ${req.body.driveId} successfully.`
+      })
         : res.json({ success: false })
     }).catch(err => {
       res.json({ success: false, error: err })
@@ -198,8 +313,9 @@ companyController.post("/deleteDrive", (req, res) => {
     CompanyDrive.deleteOne({ _id: req.body.driveId }).then(err => {
       err.deletedCount == 1 ? res.status(200).json({
         success: true,
-        message: `Deleted drive ID ${req.body.driveId} successfully.`}) 
-      : res.json({ success: false })
+        message: `Deleted drive ID ${req.body.driveId} successfully.`
+      })
+        : res.json({ success: false })
     }).catch(err => {
       res.json({ success: false, error: err })
     })
